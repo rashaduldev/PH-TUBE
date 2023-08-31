0a900e9628de41f224cfb0b9a546ebff5b9b9fe8
Rashadul Islam <rashadul.dev@gmail.com>
2023-09-01 02:29:56 +0600

    blog page Done

--- js/index.js
+++ js/index.js
@@ -13,11 +13,32 @@
 //       console.log(result);
 //   }
 
+const loadCatagory=async()=>{
+      const rea=await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
+      const data=await rea.json();
+      const catagory=data.data;
+      // allProducts(result);
+      console.log(catagory);
+      // allProducts(product);
+      allCatagory(catagory);
+  }
+  loadCatagory();
+
+  const allCatagory=catagory=>{
+      const catagoryContainer=document.getElementById('catagoryContainer');
+      catagory.forEach(catagory=>{
+            const catagoryDiv=document.createElement('div');
+            catagoryDiv.innerHTML=`
+            <button class="btn btn-ghost tab tab-active">${catagory.category}</button>
+            `
+            catagoryContainer.appendChild(catagoryDiv);
+      })
+  }
+
   const loadProducts=async()=>{
       const rea=await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
       const data=await rea.json();
       const product=data.data;
-      // allProducts(result);
       console.log(product);
       allProducts(product);
   }
@@ -31,15 +52,29 @@ const allProducts=product=>{
             const productDiv= document.createElement('div');
             productDiv.classList=`card my-6 bg-base-100 shadow-xl`;
             productDiv.innerHTML=`
-            <figure><img src="${product.thumbnail}" alt="Shoes" /></figure>
+            <figure><img class="" src="${product.thumbnail}" alt="Shoes" /></figure>
             <div class="card-body">
-              <h2 class="card-title">${product.phone_name}</h2>
-              <p>${product.slug}</p>
-              <div class="card-actions justify-center">
-                <button onclick="showDetails('${product.slug}')" class="btn btn-primary">Show Details</button>
-              </div>
+              <h2 class="card-title">${product.title}</h2>
+              <p>${product.authors[0].profile_name}</p>
+              <p>${product.others.views} views</p>
+           
+             
             </div>
             `
             productContainer.appendChild(productDiv);
       });
 }
+
+
+{/* <div class="card w-96 bg-base-100 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> */}
