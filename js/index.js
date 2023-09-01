7573879c676989b5fd2893925f9b52ceddde241c
Rashadul Islam <rashadul.dev@gmail.com>
2023-09-01 18:15:11 +0600

    code added

--- js/index.js
+++ js/index.js
@@ -14,10 +14,20 @@ const loadCatagory=async()=>{
       catagory.forEach(catagory=>{
             const catagoryDiv=document.createElement('div');
             catagoryDiv.innerHTML=`
-            <button class="btn btn-ghost tab tab-active">${catagory.category}</button>
+            <button onclick="catagoryId('${catagory.category_id}')" class="btn btn-ghost tab tab-active">${catagory.category}</button>
             `
             catagoryContainer.appendChild(catagoryDiv);
+            
       })
+  }
+
+  // catagory ID Part
+  const catagoryId=async(catagoryId)=>{
+    // console.log(catagory);
+    const response=await fetch(`https://openapi.programming-hero.com/api/videos/category/${catagoryId}`);
+    const data=await response.json();
+
+    console.log(data.data);
   }
 
   const loadProducts=async()=>{
@@ -59,18 +69,3 @@ const allProducts=product=>{
             productContainer.appendChild(productDiv);
       });
 }
-
-
-{/* <div class="card w-96 bg-base-100 shadow-xl">
-  <figure class="px-10 pt-10">
-    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-  </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div> */}
-{/* <img class="" src="${product.authors[0].profile_picture}" alt="Shoes" />   */}
