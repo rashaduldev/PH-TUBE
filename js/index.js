e1eeabb5b4b58f52e3b2005e681d085a1a1604ac
Rashadul Islam <rashadul.dev@gmail.com>
2023-09-02 20:41:34 +0600

    Assignment Done

--- js/index.js
+++ js/index.js
@@ -3,16 +3,13 @@ const loadCatagory = async () => {
   const data = await rea.json();
   const catagory = data.data;
   // allProducts(result);
-  // console.log(catagory);
+  console.log(catagory);
   // allProducts(product);
   allCatagory(catagory);
 
 }
 loadCatagory();
 
-
-
-let products = [];
 
 const allCatagory = (catagory) => {
   const catagoryContainer = document.getElementById('catagoryContainer');
@@ -28,29 +25,130 @@ const allCatagory = (catagory) => {
 };
 
 / /
-  
+
+const sortbyview = async () => {
+  // alert('hi')
+  const sortButton = document.getElementById('sortButton');
+  const catagoryId = sortButton.getAttribute("data-sortByview");
+
+  // api call 
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catagoryId}`);
+  const data = await response.json();
+  const dataRes = data.data;
+
+  const products = dataRes;
+
+  console.log(products.length);
+
+
+  const errorButton = document.getElementById('errorButton');
+  errorButton.innerText = '';
+
+  const productContainer = document.getElementById('productContainer');
+  productContainer.innerText = '';
+
+
+  if (products.length <= 0) {
+    const productIMG = document.createElement('div');
+    productIMG.innerHTML = `
+      <div>
+        <div class="grid grid-cols-1 justify-center text-center items-center m-auto lg:mt-10">
+          <p><img class="text-center mx-auto" src="../img/Icon.png" alt="" /></p>
+          <h2 class="text-3xl">Oops!! Sorry, There is no <br> content here</h2>
+        </div>
+      </div>
+    `;
+    errorButton.appendChild(productIMG);
+
+  } else {
+    // alert(products.length);
+    products.sort((a, b) => {
+      const viewOne = parseFloat(a.others.views);
+      const viewTwo = parseFloat(b.others.views);
+      return viewTwo - viewOne;
+      // return viewOne-viewTwo;   
+    })
+
+    // product loop start
+    products.forEach((product) => {
+      // 
+      const productDiv = document.createElement('div');
+      const views = product.others.views;
+
+      function timeShow(miliSeconds) {
+        const hours = Math.floor(miliSeconds / 3600);
+        const remainingSeconds = miliSeconds % 3600;
+        const minutes = Math.floor(remainingSeconds / 60);
+        return `<div>${hours}hrs ${minutes}min ago</div>`
+      }
+
+      productDiv.classList = `card my-6 bg-base-100 shadow-xl`;
+      productDiv.innerHTML = `
+        <div>
+          <div class="relative">
+            <div>
+              <figure><img class="h-80 w-full rounded-md" src="${product.thumbnail}" alt="" /></figure>
+            </div>
+            <div class="absolute bottom-0 right-0 text-white bg-black">
+              <p>${product.others.posted_date?timeShow(product.others.posted_date):""}</p>
+            </div>
+          </div>
+          <div class="flex">
+            <div>
+              <img class="w-10 h-10 rounded-full mt-7 ml-3" src="${product.authors[0].profile_picture}" alt="">
+            </div>
+            <div>
+              <div class="card-body text-left">
+                <h2 class="card-title">${product.title}</h2>
+                <div class="flex items-center gap-3">
+                  <p>${product.authors[0].profile_name}</p>
+                  ${product.authors[0].verified ? `
+                    <p>
+                      <i class="fa-solid fa-circle-check" style="color: #2568ef;"></i>
+                    </p>
+                  ` : ''}
+                </div>
+                <p>${views} views</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+      productContainer.appendChild(productDiv);
+    });
+
+  }
+
+
+  // console.log(products);
+}
+
 
 
 const catagoryId = async (catagoryId) => {
-  // console.log(catagoryId);
+  console.log(catagoryId);
+  const sortButton = document.getElementById('sortButton');
+
+  sortButton.setAttribute("data-sortByview", catagoryId);
+  // sortButton.setAttribute("data-sortByview","0118421545");
+
   const productContainer = document.getElementById('productContainer');
   productContainer.innerText = '';
   const errorButton = document.getElementById('errorButton');
   errorButton.innerText = '';
 
-  // const newArray=[];
-  // document.getElementById('sortButton').addEventListener('click', () => {
-  //   // console.log(views);
-  
-  //   // newArray.push(views);
-  //   console.log(catagoryId);
-  //   catagoryContainer.innerText = '';
-  //   // products.sort((a, b) => b.others.views - a.others.views);
-  //   // console.log(product);
-  // });
+
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catagoryId}`);
+    const data = await response.json();
+    const dataRes = data.data;
+
+    // Define and populate the products array within this scope
+    const products = dataRes;
+
+    console.log(products);
 
 
-  if (catagoryId === '1005') {
+  if (products.length <= 0) {
     const productIMG = document.createElement('div');
     productIMG.innerHTML = `
       <div>
@@ -62,46 +160,17 @@ const catagoryId = async (catagoryId) => {
     `;
     errorButton.appendChild(productIMG);
   } else {
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catagoryId}`);
-    const data = await response.json();
-    const dataRes = data.data;
-
-    // Define and populate the products array within this scope
-    const products = dataRes;
-
-    // console.log(products);
-
-
-
-
     products.forEach((product) => {
-// 
+      // 
       const productDiv = document.createElement('div');
       const views = product.others.views;
 
-      // const posted_date=product.others.posted_date;
-      // console.log(posted_date);
-      // const { hours, minutes, seconds } = convertSecondsToTime(posted_date);
-        // functionn
-    // function convertSecondsToTime(seconds) {
-    //   const hours = Math.floor(seconds / 3600);
-    //   const minutes = Math.floor((seconds % 3600) / 60);
-    //   const remainingSeconds = seconds % 60;
-    //   return { hours, minutes, seconds: remainingSeconds };
-    // }
-    // const lg=convertSecondsToTime(posted_date)
-    // console.log(lg);
-      //
       function timeShow(miliSeconds) {
-        const hours=Math.floor(miliSeconds/3600);
-        const remainingSeconds=miliSeconds%3600;
-        const minutes=Math.floor(remainingSeconds/60);
+        const hours = Math.floor(miliSeconds / 3600);
+        const remainingSeconds = miliSeconds % 3600;
+        const minutes = Math.floor(remainingSeconds / 60);
         return `<div>${hours}hrs ${minutes}min ago</div>`
       }
-
-    
-     
-
 
       productDiv.classList = `card my-6 bg-base-100 shadow-xl`;
       productDiv.innerHTML = `
